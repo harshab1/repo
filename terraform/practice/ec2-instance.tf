3706638efd1a53afa08758764e391ddfc5319ea6
harshab1 <harshabashetty1@gmail.com>
2022-04-08 00:12:12 +0100

    --

---
+++ terraform/practice/ec2-instance.tf
@@ -0,0 +1,61 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-main"
+    Project = "Terraform"
+  }
+}
+
+resource "aws_subnet" "subnets" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "190.160.1.0/24"
+
+  tags = {
+    Name = "Subnet-main-1"
+    Project = "Terraform"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SSH-SG"
+    Project = "Terraform"
+  }
+}
+
+resource "aws_instance" "sample-server" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnets.id
+  security_groups = [aws_security_group.allow_ssh.id]
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+    tags = {
+    Name = "sample-server"
+    Project = "Terraform"
+  }
+}
--- terraform/practice/vars.tf
+++ terraform/practice/vars.tf
@@ -10,17 +10,32 @@ variable "subnet_cidr" {
   default = "190.160.1.0/24"
 }
 
+variable "ami_id" {
+  default = "ami-0e472ba40eb589f49"
+}
+
+
 # variable "azs" {
 #     type = list(string)
 #     default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
 # }
 
-variable "subnet_cidrs" {
-  type = list(string)
-  default = ["190.160.1.0/24", "190.160.2.0/24", "190.160.3.0/24", "190.160.4.0/24", "190.160.5.0/24", "190.160.6.0/24"]
+variable "azs" {
+    type = list(string)
+    default = ["us-east-1a"]
 }
 
-# Declare the data source
-data "aws_availability_zones" "azs" {
-  state = "available"
+# variable "subnet_cidrs" {
+#   type = list(string)
+#   default = ["190.160.1.0/24", "190.160.2.0/24", "190.160.3.0/24", "190.160.4.0/24", "190.160.5.0/24", "190.160.6.0/24"]
+# }
+
+variable "subnet_cidrs" {
+  type = list(string)
+  default = ["190.160.1.0/24"]
 }
+
+# # Declare the data source
+# data "aws_availability_zones" "azs" {
+#   state = "available"
+# }
